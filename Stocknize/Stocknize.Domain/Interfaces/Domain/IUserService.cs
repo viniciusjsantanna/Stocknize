74c170a075d4815db908bbc4f2e1cfc777111fd0
felipenanes <felipenanes@gmail.com>
2021-05-02 23:05:46 -0300

    enviando complemento de api de users

--- Stocknize/Stocknize.Domain/Interfaces/Domain/IUserService.cs
+++ Stocknize/Stocknize.Domain/Interfaces/Domain/IUserService.cs
@@ -2,12 +2,17 @@
 using Stocknize.Domain.Models.User;
 using System.Threading;
 using System.Threading.Tasks;
+using System;
 
 namespace Stocknize.Domain.Interfaces.Domain
 {
     public interface IUserService
     {
         Task<User> AddUser(UserInputModel userModel, CancellationToken cancellationToken);
+
+        Task DeleteUser(Guid userId, CancellationToken cancellationToken);
+
+        Task<UserLoggedOutputModel> UpdateUser(Guid userId, UserInputModel userModel, CancellationToken cancellationToken);
 
         Task<UserLoggedOutputModel> Authenticate(CredentialsInputModel model, CancellationToken cancellationToken);
     }
